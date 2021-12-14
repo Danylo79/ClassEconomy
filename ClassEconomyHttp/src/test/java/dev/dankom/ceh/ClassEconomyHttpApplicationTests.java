33a6e49a361bd04c08d6f02d9ef1b7569498d306
Danylo79 <dan.komisarenko@gmail.com>
2021-12-14 09:57:26 -0500

    Added http

---
+++ ClassEconomyHttp/src/test/java/dev/dankom/ceh/ClassEconomyHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dankom.ceh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassEconomyHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
